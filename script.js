3dbd6374a13d8387416a5ecd573fbe3e68548ef9
fa1yaz <fa1yaz0khn@gmail.com>
2024-09-03 21:42:13 +0530

    first commit

--- script.js
+++ script.js
@@ -1,43 +1,58 @@
-function hamburg(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform ="translateY(0px)"
-    
+// Hamburger menu toggle
+function hamburg() {
+    const navbar = document.querySelector(".dropdown");
+    const hamburgIcon = document.querySelector(".hamburg");
+    const cancelIcon = document.querySelector(".cancel");
+    navbar.classList.add("show");
+    hamburgIcon.style.display = 'none';
+    cancelIcon.style.display = 'block';
 }
-function cancel(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform ="translateY(-500px)"
 
+function cancel() {
+    const navbar = document.querySelector(".dropdown");
+    const hamburgIcon = document.querySelector(".hamburg");
+    const cancelIcon = document.querySelector(".cancel");
+    navbar.classList.remove("show");
+    hamburgIcon.style.display = 'block';
+    cancelIcon.style.display = 'none';
 }
-//typewritter effect
-const texts =[
+
+// Typewriter effect
+const texts = [
     "PENETRATION TESTER",
     "ETHICAL HACKER"
-]
+];
 let speed = 100;
-const textElements=document.querySelector(".typewriter-text")
+const textElements = document.querySelector(".typewriter-text");
 
-let textindex=0;
-let characterindex=0;
+let textIndex = 0;
+let characterIndex = 0;
 
-function typewriter(){
-    if(characterindex< texts[textindex].length){
-        textElements.innerHTML += texts[textindex].charAt(characterindex);
-        characterindex++;
-        setTimeout(typewriter,speed);
-    }
-    else{
-        setTimeout(eraseText,1000)
+function typewriter() {
+    if (characterIndex < texts[textIndex].length) {
+        textElements.innerHTML += texts[textIndex].charAt(characterIndex);
+        characterIndex++;
+        setTimeout(typewriter, speed);
+    } else {
+        setTimeout(eraseText, 1000);
     }
 }
-function eraseText(){
-    if(textElements.innerHTML.length> 0){
-        textElements.innerHTML=textElements.innerHTML.slice(0,-1)
-        setTimeout(eraseText,50)
-    }
-    else{
-        textindex=(textindex+1)% texts.length
-        characterindex=0;
-        setTimeout(typewriter,500)
+
+function eraseText() {
+    if (textElements.innerHTML.length > 0) {
+        textElements.innerHTML = textElements.innerHTML.slice(0, -1);
+        setTimeout(eraseText, 50);
+    } else {
+        textIndex = (textIndex + 1) % texts.length;
+        characterIndex = 0;
+        setTimeout(typewriter, 500);
     }
 }
-window.onload= typewriter
+
+window.onload = typewriter;
+
+// Handle Resume download
+function showResume() {
+    const downloadLink = document.getElementById("downloadResume");
+    downloadLink.click(); // Trigger the download
+}
